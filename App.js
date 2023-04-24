3918603aec09145162bc157a8bcb77a217fc1d8c
ivandjoh <ivandi.djoh@gmail.com>
2023-04-25 06:01:26 +0800

    add bgimgcomponent

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import FullBGImageComponent from "./components/FullBGImageComponent";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome to My Expo!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <FullBGImageComponent />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
